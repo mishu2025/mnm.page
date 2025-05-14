077bc7e38cd70fc1310c60a97c45616e887ca429
mishu2025 <mintesnotnb@yahoo.com>
2025-05-14 17:22:53 +0200

    Update nn_xor_predictmodel.js

--- nn_js/nn_xor_predictmodel.js
+++ nn_js/nn_xor_predictmodel.js
@@ -3,13 +3,13 @@ async function loadAndPredict() {
   const input2 = parseFloat(document.getElementById('input2').value);
 
   if (isNaN(input1) || isNaN(input2)) {
-    //document.getElementById('output').innerText = 'Please enter valid numbers.';
+    document.getElementById('output').innerText = 'Please enter valid numbers.';
     return;
-  }
+  };
 
-  //document.getElementById('output').innerText = 'Loading model...';
+  document.getElementById('output').innerText = 'Loading model...';
   const model = await tf.loadLayersModel('../models/xor-model.json'); // hosted version
-  //document.getElementById('output').innerText = 'Model Loaded';
+  document.getElementById('output').innerText = 'Model Loaded';
 
   const inputTensor = tf.tensor2d([[input1, input2]]);
   const outputTensor = model.predict(inputTensor);
